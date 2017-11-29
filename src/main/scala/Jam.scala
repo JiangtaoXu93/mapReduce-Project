d2ee6369ba2ebbfc85a1b1e42ffa219362420bb5
Jiangtao Xu <xujiangtao93@outlook.com>
2017-11-28 19:23:06 -0500

    write part of join

---
+++ src/main/scala/Jam.scala
@@ -0,0 +1,17 @@
+class Jam(jam : String, track : String) {
+  var JAM_ID : String = jam
+  var TRACK_ID : String = track
+
+  def this(line : String) ={
+    this("","")
+    def tokens = line.split("\t")
+    this.JAM_ID = tokens(0)
+    this.TRACK_ID = tokens(1)
+  }
+
+  def getJam():String = JAM_ID
+
+  def getTrack():String = TRACK_ID
+
+
+}
--- src/main/scala/JoiningTable.scala
+++ src/main/scala/JoiningTable.scala
@@ -1,5 +1,3 @@
-import java.io.FileWriter
-
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -17,9 +15,39 @@ object JoiningTable {
     val downloads = downloadInput.mapPartitionsWithIndex { (idx, iterate) => if (idx == 0) iterate.drop(1) else iterate }
     val downloadRecord = downloads.map(row => new DownloadInfo(row)).filter(_.checkValidity())
 
-    val songInput = sc.textFile("all/song_info.csv")
+    val songInput = sc.textFile("MillionSongSubset/song_info.csv")
     val songs = songInput.mapPartitionsWithIndex { (idx, iterate) => if (idx == 0) iterate.drop(1) else iterate }
     val songRecord = songs.map(row => new SongInfo(row)).filter(_.checkValidity())
+
+
+
+
+    val tasteInput = sc.textFile("train_triplets.txt")
+    val taste = tasteInput.mapPartitionsWithIndex { (idx, iterate) => if (idx == 0) iterate.drop(1) else iterate }
+
+
+
+    val download_keyPair = downloadRecord.map(d => ((d.getArtist(),d.getTitle()), (d.getMeanPrice(),d.getDownload(),d.getConfidence())))
+      //download_keyPair is (key,value) where key -> (artist, title); value -> (price, download, confidence)
+    val songInfo_keyPair = songRecord.map(
+      s => ((s.getArtistName(),s.getTitle()),
+        (s.getTrackId(), s.getSongId(),s.getArtFam(),s.getArtHot(),s.getDuration(),s.getLoudness(),s.getSongHot(),s.getTempo())))
+      //songInfo_keyPair: same key as download_keyPair
+    val download_songInfo_join = download_keyPair.join(songInfo_keyPair)
+
+
+    val jamInput = sc.textFile("jam_to_msd.tsv")
+    val jams = jamInput.mapPartitionsWithIndex { (idx, iterate) => if (idx == 0) iterate.drop(1) else iterate }.map(new Jam(_))
+    val jamCount = jams.map(j => (j.getTrack(),j.getJam())).countByKey()
+    val jam_keyPair = sc.parallelize(jamCount.toSeq)// convert Collection[Map] to RDD
+    val download_song_keyPair = download_songInfo_join.map{
+      case (((artist:String,title:String),
+      ((price:Double,download:Int,confidence: String),(trackId: String,songId:String,famil: Double,artHot: Double,dur: Double, loud: Double,songHot:Double,tempo: Double))))
+    => (trackId,(songId,price,download,confidence,famil,artHot,dur,loud,songHot,tempo))}
+
+    val jam_download_song_join = jam_keyPair.join(download_song_keyPair)
+
+
 
 
   }
@@ -30,6 +58,15 @@ object JoiningTable {
     val download = downloadRecord.map(lines => (lines.getCombinedKey(),lines.getDownload()))
     val join = song.join(download)
   }
+
+  //select taste that has the same track id as songInfo dataset,print to csv
+  def getSubTasteData(sc: SparkContext, songRecord: RDD[SongInfo]): Unit = {
+    val tasteInput = sc.textFile("train_triplets.txt")
+    val taste = tasteInput.mapPartitionsWithIndex { (idx, iterate) => if (idx == 0) iterate.drop(1) else iterate }
+    val tasteCountTuple = taste.filter(row => isInSongInfo(row))
+  }
+
+  def
 
 }
 
