e90784453b6cedaa7cdcc349e49aef47109a530e
danielyuwen <wen.yu1@husky.neu.edu>
2017-12-07 05:39:57 -0500

    update RandomForestRegression training

--- src/main/scala/RandomForestRegression.scala
+++ src/main/scala/RandomForestRegression.scala
@@ -7,59 +7,53 @@ import org.apache.spark.ml.regression.{RandomForestRegressionModel, RandomForest
 
 object RandomForestRegression {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setMaster("local").setAppName("SPARK ML")
+    val inputDir = args(0)
+    val outputDir = args(1)
+    val mode = args(2)
+    val csvName = args(3)
+
+    // get parameters for GBT
+    val numTree = 100
+    val maxDepth = 15
+
+    //specify model names according to algorithm, csvName and its parameters
+    val modelName = "RandomForest-offJam-offTaste" +csvName +"-numTree"+numTree+"-maxDepth"+maxDepth
+
+
+    val conf = new SparkConf().setAppName(modelName)
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
-
-    // Specify dataset and model names
-    val datasetName = "dataset_32k.csv"
-    val numTree = 20
-    val maxDepth =15
-    val featureStra ="auto"
-    val modelName = "RFRegression-"+datasetName+"-numTree"+numTree+"-maxDepth"+maxDepth+"-featureStra_"+featureStra
+    val dataset = DataReader.convertCSV(csvName,inputDir,sqlContext)
+    val Array(train,test) = dataset.randomSplit(Array(0.99,0.01))
 
 
-    var dataset = sqlContext.read
-      .format("com.databricks.spark.csv")
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .load(args(0)+"/"+datasetName)
-      .distinct()
-      .toDF("songId","taste_count","jam_count", "trackId", "price","download","confidence","famil","artHot","dur","loud","songHot","tempo")
-      .cache()
-
-    val assembler = new VectorAssembler()
-        .setInputCols(Array("taste_count","jam_count", "price","famil","artHot","dur","loud","songHot","tempo"))
-        .setOutputCol("features")
-    dataset = assembler.transform(dataset).cache()
-    dataset = dataset.select("download","features").cache()
-    dataset = dataset.withColumnRenamed("download","label").cache()
-
-    val Array(train,test) = dataset.randomSplit(Array(0.8,0.2))
-
-//    println("Start training models.....")
-//    val rf = new RandomForestRegressor()
-//      .setLabelCol("label")
-//      .setFeaturesCol("features")
-//      .setMaxDepth(maxDepth)
-//      .setFeatureSubsetStrategy(featureStra)
-//      .setNumTrees(numTree)
-//    val model = rf.fit(train)
-//    model.save(args(1)+"/"+modelName)
-
+    if(mode == "training") {
+      println("Start training models.....")
+      val featureStra ="auto"
+      val rf = new RandomForestRegressor()
+        .setLabelCol("label")
+        .setFeaturesCol("features")
+        .setMaxDepth(maxDepth)
+        .setFeatureSubsetStrategy(featureStra)
+        .setNumTrees(numTree)
+      val model = rf.fit(train)
+      model.save(outputDir+"/"+modelName)
+    }
 
     println("Loading pretrianed model")
-    val model = RandomForestRegressionModel.load(args(1)+"/"+modelName)
-    // Make predictions.
-    val predictions = model.transform(test)
+    val trained_model = RandomForestRegressionModel.load(outputDir+"/"+modelName)
+
+    println("Making predictions")
+    val predictions = trained_model.transform(test)
 
     val evaluator = new RegressionEvaluator()
       .setLabelCol("label")
       .setPredictionCol("prediction")
       .setMetricName("rmse")
     val rmse = evaluator.evaluate(predictions)
-    println("Dataset size "+ dataset.count())
+
+    println("Testset size "+test.count())
     println("Root Mean Squared Error (RMSE) on test data = " + rmse)
 
 
