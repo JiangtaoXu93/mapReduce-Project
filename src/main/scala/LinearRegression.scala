d80d0852e46ca72e09b11e07b74168aa7f507632
danielyuwen <wen.yu1@husky.neu.edu>
2017-12-07 05:40:16 -0500

    update linear regression

--- src/main/scala/LinearRegression.scala
+++ src/main/scala/LinearRegression.scala
@@ -1,63 +1,49 @@
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.ml.regression.{LinearRegressionModel, LinearRegression}
-import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 
 object LinearRegression {
 
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("LinearRegression")
+    val inputDir = args(0)
+    val outputDir = args(1)
+    val mode = args(2)
+    val csvName = args(3)
+
+    //specify model names according to algorithm
+    val modelName = "LinearRegression-offJam-offTaste" + csvName
+
+    val conf = new SparkConf().setAppName(modelName)
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
-    // Specify dataset and model names
-    val datasetName = "dataset_32k.csv"
+    val dataset = DataReader.convertCSV(csvName, args(0), sqlContext)
+    val Array(train, test) = dataset.randomSplit(Array(0.99, 0.01))
 
-    val modelName = "LinearRegression-" + datasetName
-
-    var dataset = sqlContext.read
-      .format("com.databricks.spark.csv")
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .load(args(0) + "/" + datasetName)
-      .distinct()
-      .toDF("songId", "taste_count", "jam_count", "trackId", "price", "download", "confidence", "famil", "artHot", "dur", "loud", "songHot", "tempo")
-      .cache()
-
-    val assembler = new VectorAssembler()
-      .setInputCols(Array("taste_count", "jam_count", "price", "famil", "artHot", "dur", "loud", "songHot", "tempo"))
-      .setOutputCol("features")
-    dataset = assembler.transform(dataset).cache()
-    dataset = dataset.select("download", "features").cache()
-    dataset = dataset.withColumnRenamed("download", "label").cache()
-
-
-    val Array(train, test) = dataset.randomSplit(Array(0.8, 0.2))
-
-
-//    println("Start training models.....")
-//    val lr = new LinearRegression()
-//    val model = lr.fit(train)
-//    model.save(args(1) + "/" + modelName)
-
+    if (mode == "training") {
+      println("Start training models.....")
+      val lr = new LinearRegression()
+      val model = lr.fit(train)
+      model.save(outputDir + "/" + modelName)
+    }
 
     println("Loading pre-trained model")
-    val model =  LinearRegressionModel.load(args(1)+"/"+modelName)
+    val trained_model = LinearRegressionModel.load(outputDir + "/" + modelName)
 
     println("Making predictions")
-    val predictions = model.transform(test)
+    val predictions = trained_model.transform(test)
 
     val evaluator = new RegressionEvaluator()
       .setLabelCol("label")
       .setPredictionCol("prediction")
       .setMetricName("rmse")
     val rmse = evaluator.evaluate(predictions)
-    println("Dataset size " + dataset.count())
+
+    println("Dataset size " + test.count())
     println("Root Mean Squared Error (RMSE) on test data = " + rmse)
 
   }
-
 
 
 }
